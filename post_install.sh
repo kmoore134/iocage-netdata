3cadd697d2b47190d50caeb5f68b9536601ac838
Kris Moore <kris@ixsystems.com>
2020-02-16 06:13:24 -0800

    Bind to all addresses

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+cat /usr/local/etc/netdata/netdata.conf | grep -v "bind to" | > /tmp/netdata.conf
+mv /tmp/netdata.conf /usr/local/etc/netdata/netdata.conf
 sysrc -f /etc/rc.conf netdata_enable=YES
 service netdata start
